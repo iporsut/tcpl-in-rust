e7447ea2b6d1f3257feb1b77b689ef90edc9bff1
Weerasak Chongnguluam <singpor@gmail.com>
2017-11-16 23:38:40 +0700

    use map and match

--- rlc/src/main.rs
+++ rlc/src/main.rs
@@ -2,11 +2,11 @@ use std::io;
 use std::io::Read;
 
 fn main() {
-    let mut nl = 0;
-    for c in io::stdin().bytes() {
-        if c.unwrap() == '\n' as u8 {
-            nl += 1;
-        }
-    }
-    println!("{}", nl);
+    println!("{}", io::stdin()
+             .bytes()
+             .map(|r| match r.map(|c| c == '\n' as u8) {
+                 Ok(true) => 1,
+                 _        => 0
+             })
+             .sum::<i32>());
 }
