8b4e483f94b7fb55b7cf7acd3607c84fddd32ffb
Weerasak Chongnguluam <singpor@gmail.com>
2017-11-17 00:29:01 +0700

    ex1-8 use fold

--- ex1-8/src/main.rs
+++ ex1-8/src/main.rs
@@ -2,19 +2,21 @@ use std::io;
 use std::io::Read;
 
 fn main() {
-    let mut bc = 0;
-    let mut nc = 0;
-    let mut tc = 0;
+    let (bc, tc, nc) = io::stdin()
+        .bytes()
+        .fold((0, 0, 0), |acc, r| {
+            let (bc, tc, nc) = acc;
+            match r {
+                Ok(c)  => match c as char {
+                    ' '  => (bc + 1, tc, nc),
+                    '\t' => (bc, tc + 1, nc),
+                    '\n' => (bc, tc, nc + 1),
+                    _    => acc
+                },
+                _      => acc
 
-    for r in io::stdin().bytes() {
-        let c = r.unwrap();
-        match c as char {
-            '\n' => { nc += 1; },
-            '\t' => { tc += 1; },
-            ' '  => { bc += 1; },
-            _    => {}
-        }
-    }
+            }
+        });
 
     println!("{} {} {}", bc, tc, nc);
 }
