1ab667d40db0036b21f4d5b9cf65746ea292237c
Weerasak Chongnguluam <singpor@gmail.com>
2017-11-17 09:22:07 +0700

    use lock lines count

--- rlc/src/main.rs
+++ rlc/src/main.rs
@@ -1,12 +1,6 @@
 use std::io;
-use std::io::Read;
-
+use std::io::BufRead;
 fn main() {
-    println!("{}", io::stdin()
-             .bytes()
-             .map(|r| match r.map(|c| c == '\n' as u8) {
-                 Ok(true) => 1,
-                 _        => 0
-             })
-             .sum::<i32>());
+    let stdin = io::stdin();
+    println!("{}", stdin.lock().lines().count());
 }
